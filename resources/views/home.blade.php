4cb672f6fbe807a01209638d9a4c11249ef87447
Jhon <jperalta@mescalina.mx>
2024-10-14 19:50:21 -0600

    Ajustes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <h1 class="logo">Aqui estará el inicio y logo de la pagina del Tecnológico</h1>
     <div class="container">
         <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
           <div class="col-md-3 mb-2 mb-md-0">
