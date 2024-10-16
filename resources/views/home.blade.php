a618e0433ba238bea4b86d326f11f9e6efa67e1e
Jhon <jperalta@mescalina.mx>
2024-10-16 16:20:02 -0600

    Modificacion de la pagina principal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,8 @@
   <title>Página Principal</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="stylesHome.css">
-  {{-- <link rel="stylesheet" href="{!! asset('css/stylesHome.css') !!}">
-  <link rel="stylesheet" href="{{asset('./../css/styles.css')}}"> --}}
+  <link rel="stylesheet" href="{!! asset('css/stylesHome.css') !!}">
+  <link rel="stylesheet" href="{{asset('./../css/styles.css')}}">
 
   
 </head>
